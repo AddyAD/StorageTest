7bfc59dd057e275e430c1496c1ec8840c82a3f5b
ad <ad@itmp.top>
2015-10-02 16:50:32 +0800

    well-done

--- app/src/main/java/com/example/hello/storagetest/MainActivity.java
+++ app/src/main/java/com/example/hello/storagetest/MainActivity.java
@@ -1,37 +1,46 @@
 package com.example.hello.storagetest;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+import android.content.res.Resources;
 
 public class MainActivity extends Activity {
+    private Button btnRead=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnRead = (Button)super.findViewById(R.id.read);
+        btnRead.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Resources res = MainActivity.this.getResources();
+                InputStream input = res.openRawResource(R.raw.friend);
+                Scanner scan = new Scanner(input);
+                StringBuffer info=new StringBuffer();
+                while(scan.hasNext())
+                    info.append(scan.next()).append("\n");
+                scan.close();
+                try{
+                    input.close();
+                }catch(IOException e)
+                {
+                    e.printStackTrace();
+                }
+                Toast.makeText(getApplicationContext(),info.toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
